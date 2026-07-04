b4c9d3f49ca8c3f3270acccafacb71e3cb74aa59
Prabhav Vanguri <idemandataco@gmail.com>
2026-07-04 14:58:42 -0500

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Prabhav Vanguri on 7/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WorkoutAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WorkoutAppTests/WorkoutAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppTests.swift
+//  WorkoutAppTests
+//
+//  Created by Prabhav Vanguri on 7/4/26.
+//
+
+import Testing
+@testable import WorkoutApp
+
+struct WorkoutAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
